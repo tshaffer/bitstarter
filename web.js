040ecd95e93dadfa1f60c398f62345801860475f
Ubuntu <ubuntu@ip-172-31-27-149.us-west-2.compute.internal>
2013-07-27 19:53:14 +0000

    Require fs

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require("fs");
+
 app.get('/', function(request, response) {
   var buf = fs.readFileSync("index.html");
   response.send(buf.toString());
